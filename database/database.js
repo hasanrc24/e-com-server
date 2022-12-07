6775af07133f378cc387bbff6c72ef2bebb7b1be
hasanrc24 <hasanrc24@gmail.com>
2022-12-07 20:53:56 +0600

    database

--- database/database.js
+++ database/database.js
@@ -4,8 +4,8 @@ uri =
   "mongodb+srv://e-com:wPcn9pfGdUboOk34@cluster0.xflbu.mongodb.net/e-com?retryWrites=true&w=majority";
 
 const connectDB = () => {
-  console.log("DB");
-  return mongoose.mongoose.set("strictQuery", false).connect(uri, {
+  console.log("ok");
+  return mongoose.set("strictQuery", false).connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
